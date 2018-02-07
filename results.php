453e883228f9e1d84202368fa64ea695c40a148f
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-02-07 16:11:54 +0100

    Har gjort 4 classer

--- results.php
+++ results.php
@@ -14,10 +14,42 @@
 <?php
 
 
-echo "<img src='./images/apa.jpg' height='250px;' onclick='alert(\"ap-ljud\")'; class='apa'/>";
-echo "<img src='./images/giraff.jpg' height='250px;' onclick='alert(\"giraff-ljud\")'; class='giraff'/>";
-echo "<img src='./images/tiger.jpg' height='250px;' onclick='alert(\"tiger-ljud\")'; class='tiger'/>";
-echo "<img src='./images/kokos.jpg' height='250px;'>";
+
+class Apa {
+    public function draw() {
+        echo "<img src='./images/apa.jpg' height='250px;' onclick='alert(\"ap-ljud\")'; class='apa'/>";
+    }
+}
+
+class Giraff {
+    public function draw() {
+        echo "<img src='./images/giraff.jpg' height='250px;' onclick='alert(\"giraff-ljud\")'; class='giraff'/>";
+    }
+}
+
+class Tiger {
+    public function draw() {
+        echo "<img src='./images/tiger.jpg' height='250px;' onclick='alert(\"tiger-ljud\")'; class='tiger'/>";
+    }
+}
+
+class Kokos {
+    public function draw() {
+        echo "<img src='./images/kokos.jpg' height='250px;'>";
+    }
+}
+
+
+
+$apa = new Apa();
+$giraff = new Giraff();
+$tiger = new Tiger();
+$kokos = new Kokos();
+
+$apa -> draw();
+$giraff -> draw();
+$tiger -> draw();
+$kokos -> draw();
 
 
 ?>
