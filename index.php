e80e621aa5e974dc5b5e5161521c2a98b067d0fe
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-02-07 14:28:33 +0100

    skapat form

--- index.php
+++ index.php
@@ -3,18 +3,38 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Naturreservat</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
 
+<form method="GET" action="results.php">
+    Apor:<br>
+    <input type="number" name="apa" value="">
+    <br>
+    Giraffer:<br>
+    <input type="number" name="giraff" value="">
+    <br>
+    Tigrar:<br>
+    <input type="number" name="tiger" value="">
+    <br>
+    Kokosnötter:<br>
+    <input type="number" name="kokos" value="">
+    <br><br>
+    <input type="submit" value="Skicka">
+<form>
+
+
+
 <?php
 
 
 
-echo ("Hello world!");
+
+
+
 
 
 
