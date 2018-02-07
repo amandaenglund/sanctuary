aa59ed0823550f29a97cf3fc6fbd90bc35f46a9b
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-02-07 15:36:56 +0100

    Nu alertas ett ljud ut på varje djur

--- results.php
+++ results.php
@@ -1,5 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Resultat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+
 <?php
 
 
+echo "<img src='./images/apa.jpg' height='250px;' onclick='alert(\"ap-ljud\")'; class='apa'/>";
+echo "<img src='./images/giraff.jpg' height='250px;' onclick='alert(\"giraff-ljud\")'; class='giraff'/>";
+echo "<img src='./images/tiger.jpg' height='250px;' onclick='alert(\"tiger-ljud\")'; class='tiger'/>";
+echo "<img src='./images/kokos.jpg' height='250px;'>";
+
 
 ?>
+    
+</body>
+</html>
